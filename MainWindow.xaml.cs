33b021c33d85941abb701423899325893329c9a6
vlad kulakov <vlad_kulakov_01@mail.ru>
2024-05-18 16:33:15 +0500

    Revert "Test create "hello string""
    This reverts commit 6752c1ed993a47a8325de3cc904e6a0c64e441fd.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,6 @@ namespace VoiceClient
         private WaveFileWriter waveFile = null;
         private List<WaveFileWriter> waveFiles = new List<WaveFileWriter>();
         private string outputFileName;
-        private string hello;
 
         private DispatcherTimer timer = new DispatcherTimer(); // Таймер для отсчета времени записи
         private TimeSpan recordingTime; // Время записи
