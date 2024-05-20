9b368fc095a7d17164aa61567bc1ddc1940ffb86
vlad kulakov <vlad_kulakov_01@mail.ru>
2024-05-20 14:22:25 +0500

    Рефакторинг. Выделен с основного кода класс AudioClass, где реализовано 1. Получение длительности аудиофайла 2. Старт запись 3. Стоп запись 4. Проигрывание записи

---
+++ AudioClass.cs
@@ -0,0 +1,167 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VoiceClient
+{
+    public class AudioClass
+    {
+        private WaveInEvent waveSource = null;
+        private WaveFileWriter waveFile = null;
+        public string GetFileDuration(WaveFileWriter waveFile)
+        {
+            try
+            {
+                if (waveFile != null)
+                {
+                    waveFile.Dispose(); // здесь освобождаем ресурсы чтобы им можно было воспользоваться далее или что-то типа такого
+
+                    using (WaveFileReader reader = new WaveFileReader(waveFile.Filename))
+                    {
+                        TimeSpan duration = reader.TotalTime;
+                        return duration.ToString(@"hh\:mm\:ss");
+                    }
+                }
+                return "N/A";
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+                return "N/A";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+                return "N/A";
+            }
+        }
+
+        public void StartRecordAudio(string outputFileName, List<WaveFileWriter> WaveFiles = null)
+        {
+            try
+            {
+                if (waveSource == null)
+                {
+                    outputFileName = "audio#" + Guid.NewGuid().ToString();
+
+                    waveSource = new WaveInEvent();
+                    waveSource.WaveFormat = new WaveFormat(44100, 1);
+                    waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+                    waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+                    waveFile = new WaveFileWriter(outputFileName, waveSource.WaveFormat);
+
+                    if (WaveFiles != null)
+                        WaveFiles.Add(waveFile);
+
+                    waveSource.StartRecording();
+                }
+                else
+                {
+                    MessageBox.Show("Запись уже в процессе.");
+                }
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+            }
+        }
+
+        public void StopRecord()
+        {
+            try
+            {
+                if (waveSource != null)
+                {
+                    waveSource.StopRecording();
+                }
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+            }
+        }
+
+        public void PlayAudio(string outputFileName)
+        {
+            try
+            {
+                WaveFileReader fileReader = new WaveFileReader(outputFileName);
+                WaveOutEvent waveOut = new WaveOutEvent();
+                waveOut.Init(fileReader);
+                do
+                {
+                    waveOut.Play();
+                }
+                while (waveOut.PlaybackState == PlaybackState.Playing);
+
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+            }
+        }
+        private void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                if (waveFile != null)
+                {
+                    waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                    waveFile.Flush();
+                }
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+            }
+        }
+        private void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            try
+            {
+                if (waveSource != null)
+                {
+                    waveSource.Dispose();
+                    waveSource = null;
+                }
+
+                if (waveFile != null)
+                {
+                    waveFile.Dispose();
+                    waveFile = null;
+                }
+            }
+            catch (NAudio.MmException naudioex)
+            {
+                MessageBox.Show($"Ошибка в MmException: {naudioex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка в Exception: {ex.Message}");
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,9 +11,8 @@ namespace VoiceClient
 {
     public partial class MainWindow : Window
     {
-        private WaveInEvent waveSource = null;
-        private WaveFileWriter waveFile = null;
-        private List<WaveFileWriter> waveFiles = new List<WaveFileWriter>();
+        AudioClass AC = new AudioClass();
+        private List<WaveFileWriter> WaveFiles = new List<WaveFileWriter>();
         private string outputFileName;
 
         private DispatcherTimer timer = new DispatcherTimer();
@@ -30,18 +29,20 @@ namespace VoiceClient
             timer.Tick += Timer_Tick;
             recordingTime = TimeSpan.Zero;
             timerTextBlock.Text = recordingTime.ToString(@"hh\:mm\:ss");
-            StartRecordAudio();
             timer.Start();
+
+            AC.StartRecordAudio(outputFileName, WaveFiles);
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             timer.Tick -= Timer_Tick;
-            StopRecord();
+            AC.StopRecord();
+            UpdateListView();
         }
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            PlayAudio();
+            AC.PlayAudio(outputFileName);
         }
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -51,7 +52,7 @@ namespace VoiceClient
                 {
                     dynamic selectedFile = listView.SelectedItem; // Получение выбранного элемента из ListView
                     string selectedFileName = selectedFile.FileName; // Получение имени выбранного файла
-                    string filePathToPlay = waveFiles.Find(wf => wf.Filename == selectedFileName)?.Filename; // Поиск соответствующего пути к файлу
+                    string filePathToPlay = WaveFiles.Find(wf => wf.Filename == selectedFileName)?.Filename; // Поиск соответствующего пути к файлу
                     if (filePathToPlay != null)
                         outputFileName = filePathToPlay;
                 }
@@ -62,152 +63,25 @@ namespace VoiceClient
             }
         }
 
-        private async Task UpdateListView()
-        {
-            listView.Items.Clear();
-
-            foreach (WaveFileWriter file in waveFiles)
-            {
-                listView.Items.Add(new
-                {
-                    FileName = file.Filename,
-                    Duration = GetFileDuration(file.Filename) // Метод для получения длительности файла
-                });
-            }
-        }
-
-        private string GetFileDuration(string fileName)
-        {
-            // Здесь можно добавить логику для определения длительности записанного файла
-            // Например, использовать NAudio или другие библиотеки для работы с аудио
-            // Возвратим пока что просто "N/A"
-            return "N/A";
-        }
-
-        private void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        private void UpdateListView()
         {
             try
             {
-                if (waveFile != null)
+                listView.Items.Clear();
+
+                foreach (WaveFileWriter file in WaveFiles)
                 {
-                    waveFile.Write(e.Buffer, 0, e.BytesRecorded);
-                    waveFile.Flush();
+                    listView.Items.Add(new
+                    {
+                        FileName = file.Filename,
+                        Duration = AC.GetFileDuration(file) // Метод для получения длительности файла
+                    });
                 }
-            }
-            catch (NAudio.MmException naudioex)
-            {
-                MessageBox.Show(naudioex.Message);
-            }
-            catch (Exception ex)
+            }catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-        }
-        private void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
-        {
-            try
-            {
-                if (waveSource != null)
-                {
-                    waveSource.Dispose();
-                    waveSource = null;
-                }
-
-                if (waveFile != null)
-                {
-                    waveFile.Dispose();
-                    waveFile = null;
-                }
-            }
-            catch (NAudio.MmException naudioex)
-            {
-                MessageBox.Show(naudioex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void StartRecordAudio()
-        {
-            try
-            {
-                if (waveSource == null)
-                {
-                    outputFileName = "audio#" + Guid.NewGuid().ToString();
-
-                    waveSource = new WaveInEvent();
-                    waveSource.WaveFormat = new WaveFormat(44100, 1);
-                    waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
-                    waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
-
-                    waveFile = new WaveFileWriter(outputFileName, waveSource.WaveFormat);
-                    waveFiles.Add(waveFile);
-
-                    waveSource.StartRecording();
-
-                    UpdateListView();
-                }
-                else
-                {
-                    MessageBox.Show("Запись уже в процессе.");
-                }
-            }
-            catch (NAudio.MmException naudioex)
-            {
-                MessageBox.Show(naudioex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void StopRecord()
-        {
-            try
-            {
-                if (waveSource != null)
-                {
-                    timer.Stop();
-                    waveSource.StopRecording();
-                }
-            }
-            catch (NAudio.MmException naudioex)
-            {
-                MessageBox.Show(naudioex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void PlayAudio()
-        {
-            try
-            {
-                WaveFileReader fileReader = new WaveFileReader(outputFileName);
-                WaveOutEvent waveOut = new WaveOutEvent();
-                waveOut.Init(fileReader);
-                do
-                {
-                    waveOut.Play();
-                }
-                while (waveOut.PlaybackState == PlaybackState.Playing);
-
-                waveOut.Dispose();
-                waveOut = null;
-            }
-            catch (NAudio.MmException naudioex)
-            {
-                MessageBox.Show(naudioex.Message);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            
         }
 
         private void Timer_Tick(object sender, EventArgs e)
